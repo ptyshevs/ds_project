ea3af24705321a0e9e28c95c83b957815d0eb23a
Mykola Zomchak <mykola99ds@gmail.com>
2019-07-31 18:43:13 +0300

    Create 2 questions

--- web/__init__.py
+++ web/__init__.py
@@ -8,7 +8,6 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
@@ -26,10 +25,4 @@ def create_app(test_config=None):
 
     from . import form
     app.register_blueprint(form.bp)
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     return app
