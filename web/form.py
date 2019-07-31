bbfc6454b5ac7cb51bb3661a5189507d5553446a
Volodymyr Butko <turb0bur@ukr.net>
2019-07-31 18:45:28 +0300

    Added common layout, front page, separated styles and scripts files; Changed routes

--- web/form.py
+++ web/form.py
@@ -4,9 +4,14 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
-bp = Blueprint('form', __name__, url_prefix='/form')
+bp = Blueprint('form', __name__, url_prefix='')
 
 
-@bp.route('', methods=('GET', 'POST'))
+@bp.route('/')
+def hello():
+    return render_template('hello.html')
+
+
+@bp.route('/survey', methods=('GET', 'POST'))
 def form():
     return render_template('form.html' if request.method == 'GET' else 'prediction.html')
