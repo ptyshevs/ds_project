125958ff734ed0b056767fdc3f6b98d4c2b317bd
Mykola Zomchak <mykola99ds@gmail.com>
2019-08-01 12:25:12 +0300

    almost finish second page

--- web/static/script.js
+++ web/static/script.js
@@ -1,8 +1,24 @@
 const ageBtn = $('#age-btn');
 const ageContainer = $('#age');
 const countryContainer = $('#country');
+const countryBtn = $('#country-btn');
+const industryContainer = $('#industry');
+const industryBtn = $('#industry-btn');
+const roleContainer = $('#role');
+const roleBtn = $('#role-btn');
+const experienceContainer = $('#experience');
+const experienceBtn = $('#experience-btn');
+const activitiesContainer = $('#activities');
 
-ageBtn.click(function(){
-    ageContainer.hide();
-    countryContainer.removeAttr('hidden');
-});
+function assign(btn, toHide, toShow) {
+    btn.click(function () {
+        toHide.hide();
+        toShow.removeAttr('hidden');
+    });
+}
+
+assign(ageBtn, ageContainer, countryContainer);
+assign(countryBtn, countryContainer, industryContainer);
+assign(industryBtn, industryContainer, roleContainer);
+assign(roleBtn, roleContainer, experienceContainer);
+assign(experienceBtn, experienceContainer, activitiesContainer);
