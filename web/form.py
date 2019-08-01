6141afa1bd390b3104f922022962bedce715607b
Mykola Zomchak <mykola99ds@gmail.com>
2019-08-01 20:12:39 +0300

    change salary model import for heroku

--- web/form.py
+++ web/form.py
@@ -1,6 +1,6 @@
 from flask import (Flask, render_template, request)
 
-from web.salary_prediction import SalaryModel
+from salary_prediction import SalaryModel
 
 app = Flask(__name__)
 model = SalaryModel()
@@ -24,8 +24,13 @@ def form():
         activities = request.form.to_dict(flat=False)['activities']
 
         sample = model.form_input_to_sample(age, country, industry, role, experience, activities)
+
+        buckets = ['0-10,000', '10-20,000', '20-30,000', '30-40,000', '40-50,000',
+         '50-60,000', '60-70,000', '70-80,000', '80-90,000', '90-100,000',
+         '100-125,000', '125-150,000', '150-200,000', '200-250,000', '>250,000']
+
         (salary_index, dist) = model.predict(sample)
-        return render_template('prediction.html', salary=salary_index, salary_distribution=dist)
+        return render_template('prediction.html', salary=buckets[salary_index], salary_distribution=dist)
 
 
 @app.route('/recommendation', methods=('GET', 'POST'))
